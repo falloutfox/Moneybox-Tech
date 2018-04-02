cd764ee0f0d49fa106ec05ebbd6aad6d31e2bb90
Ollie <ollie@bluejuice.co.uk>
2018-04-02 18:00:18 +0100

    Added Session.swift

---
+++ Moneybox Tech/Objects/Session.swift
@@ -0,0 +1,22 @@
+//
+//  Session.swift
+//  Moneybox Tech
+//
+//  Created by Ollie Stowell on 29/03/2018.
+//  Copyright © 2018 Oliver Stowell. All rights reserved.
+//
+
+import Foundation
+
+///An object that holds the data needed to authorise a session
+class Session: Decodable {
+	///The Bearer Token for authorising requests
+	private(set) public var bearerToken: String = ""
+	
+	//Used to decode incoming JSON into the correct properties
+	enum CodingKeys: String, CodingKey {
+		case bearerToken = "BearerToken"
+	}
+	
+	//MARK: -
+}
